6f1649ce0ee46748186f77c4485ef51673b380fc
Amitrajit Sarkar <aaiijmrtt@gmail.com>
2017-02-14 00:46:23 +0530

    configure for texfiles

--- .vimrc
+++ .vimrc
@@ -6,6 +6,17 @@
 :set autoindent " align newline indents
 :set mouse=a " enable mouse usage
 :set whichwrap+=<,>,h,l,[,] " enable cursor line wrapping
+:set nocompatible
+
+" linewrap & navigation
+
+nmap <up> gk
+nmap <down> gj
+augroup WrapLineInTeXFile
+	autocmd!
+	autocmd FileType tex setlocal wrap
+	autocmd FileType tex setlocal textwidth=80
+augroup END
 
 " pathogen
 execute pathogen#infect()
@@ -15,3 +26,14 @@ let NERDTreeIgnore=['\.o$','\.class$','\.pyc$']
 
 " nerdtree tabs
 let g:nerdtree_tabs_open_on_console_startup=1
+
+" airline
+let g:airline#extensions#tabline#enabled=1
+
+" lexical
+
+filetype plugin on
+augroup lexical
+	autocmd!
+	autocmd FileType tex call lexical#init()
+augroup END
--- install.sh
+++ install.sh
@@ -6,13 +6,13 @@ git clone https://github.com/tpope/vim-pathogen ~/.vim
 mkdir ~/.vim/bundle
 
 git clone https://github.com/scrooloose/nerdtree ~/.vim/bundle/nerdtree
+git clone https://github.com/jistr/vim-nerdtree-tabs ~/.vim/bundle/vim-nerdtree-tabs
 git clone https://github.com/tpope/vim-fugitive ~/.vim/bundle/vim-fugitive
 git clone https://github.com/raimondi/delimitmate ~/.vim/bundle/delimitmate
 git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
-git clone https://github.com/jistr/vim-nerdtree-tabs ~/.vim/bundle/vim-nerdtree-tabs
 git clone https://github.com/xuyuanp/nerdtree-git-plugin ~/.vim/bundle/nerdtree-git-plugin
 git clone https://github.com/airblade/vim-gitgutter ~/.vim/bundle/vim-gitgutter
-
+git clone https://github.com/reedes/vim-lexical ~/.vim/bundle/vim-lexical
 git clone https://github.com/valloric/youcompleteme ~/.vim/bundle/youcompleteme
 cd ~/.vim/bundle/youcompleteme
 git submodule update --init --recursive
